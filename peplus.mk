bd8f1d04d62c861a20fa1d1970b75ed6614612c7
Julev Ajeto <jersoncarin25@gmail.com>
2022-06-24 19:29:35 +0800

    Initial release

---
+++ peplus.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
+$(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
+-include vendor/aosp/build/core/config.mk
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+PRODUCT_PACKAGES += \
+    libaptX_encoder \
+    libaptXHD_encoder
